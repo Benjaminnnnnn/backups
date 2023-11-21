32251755498b772777c1d64a4d52d7e6acc99c92
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-11-20 18:01:41 -0800

    updated config to enable auto rename & close tag

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -123,6 +123,31 @@ local plugins = {
     "wakatime/vim-wakatime",
     lazy = false,
   },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "html",
+      "javascript",
+      "typescript",
+      "javascriptreact",
+      "typescriptreact",
+      "svelte",
+      "vue",
+      "tsx",
+      "jsx",
+      "rescript",
+      "xml",
+      "php",
+      "markdown",
+      "astro",
+      "glimmer",
+      "handlebars",
+      "hbs",
+    },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
 
 return plugins
