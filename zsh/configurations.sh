4cec9a1b7901dd3b2fa3e51479ee49ca31308e33
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-03 20:36:10 -0800

    Sync configs

--- zsh/configurations.sh
+++ zsh/configurations.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # fzf
-export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob '!.git' --glob '!node_modules'"
+export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --ignore-file $HOME/.gitignore"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="
   --preview 'bat -n --color=always --theme=OneHalfDark {}'
