cd8815dc75c809262aa1017c4f29589f09ca4f13
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-03 20:53:13 -0800

    Sync configs

--- zsh/configurations.sh
+++ zsh/configurations.sh
@@ -1,9 +1,7 @@
 #!/bin/zsh
 
 # fzf
-IGNORED_FILES={.git,node_modules}
-# export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob $(!{.git,node_modules}) --ignore-file $HOME/.gitignore"
-export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob !$IGNORED_FILES --ignore-file $HOME/.gitignore"
+export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob !.git --glob !node_modules --ignore-file $HOME/.gitignore"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="
   --preview 'bat -n --color=always --theme=OneHalfDark {}'
