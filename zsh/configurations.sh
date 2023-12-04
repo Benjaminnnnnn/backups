df27745f6506b27624dee160a947c4bb69310d21
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-03 20:23:15 -0800

    Sync configs

--- zsh/configurations.sh
+++ zsh/configurations.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
 # fzf
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow'
 export FZF_CTRL_T_OPTS="
   --preview 'bat -n --color=always --theme=OneHalfDark {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|)'"
