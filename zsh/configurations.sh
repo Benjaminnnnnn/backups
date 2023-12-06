770692954a4676e437940654519f23ce4335d0c8
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-05 23:24:27 -0800

    Sync configs

--- zsh/configurations.sh
+++ zsh/configurations.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 # fzf
-export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob !.git --glob !node_modules --ignore-file $HOME/.gitignore"
+export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob !.git --glob !node_modules --ignore-file $HOME/.gitignore 2>/dev/null"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="
   --preview 'bat -n --color=always --theme=OneHalfDark {}'
-  --bind 'ctrl-/:change-preview-window(down|hidden|)' 2>/dev/null"
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
