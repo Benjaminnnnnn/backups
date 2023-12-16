a0a778d372ba3f5e712573008b663f49c0ee0db4
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-16 00:29:22 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -152,8 +152,12 @@ local plugins = {
     "kdheepak/lazygit.nvim",
     -- optional for floating window border decoration
     dependencies = {
+      "nvim-telescope/telescope.nvim",
       "nvim-lua/plenary.nvim",
     },
+    config = function()
+      require("telescope").load_extension "lazygit"
+    end,
   },
 }
 
