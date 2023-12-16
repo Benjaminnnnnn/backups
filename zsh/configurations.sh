6d3bc0bcf9dc07f178fcce4d37c6989fd419866c
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-15 23:25:45 -0800

    Sync configs

--- zsh/configurations.sh
+++ zsh/configurations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+# fzf
 RG_PREFIX="rg --files --hidden --follow --glob !.git --glob !node_modules --ignore-file $HOME/.gitignore --column --line-number --no-heading --smart-case"
 
-# fzf
 export FZF_DEFAULT_COMMAND="$RG_PREFIX 2>/dev/null"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="
