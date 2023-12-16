c6b6449116704c21a129771bcf03cb6fb861d13c
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-16 00:27:52 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -148,6 +148,13 @@ local plugins = {
       require("nvim-ts-autotag").setup()
     end,
   },
+  {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
 
 return plugins
