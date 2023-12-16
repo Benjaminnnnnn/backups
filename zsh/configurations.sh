4a2ac6609151400f91dcdeeefb105e056ed4fd5f
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-16 02:00:00 -0800

    Sync configs

--- zsh/configurations.sh
+++ zsh/configurations.sh
@@ -6,5 +6,5 @@ RG_PREFIX="rg --files --hidden --follow --glob !.git --glob !node_modules --igno
 export FZF_DEFAULT_COMMAND="$RG_PREFIX 2>/dev/null"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="
-  --preview 'bat -n --color=always --theme=Nord {}'
+  --preview 'bat -n --color=always --theme=OneHalfDark {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|),ctrl-d:reload(find . -type d),ctrl-f:reload(eval $FZF_DEFAULT_COMMAND)'"
