1bb8165f652b9b0714aa73887ddaddcadeb72757
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-23 09:40:33 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -162,7 +162,9 @@ local plugins = {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    lazy = false,
+    init = function()
+      require "todo-comments"
+    end,
   },
 
   { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
