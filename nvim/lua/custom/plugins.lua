2a68cb4e35ad21d9e4c9ec43e94883617e42559a
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-23 09:35:19 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -162,7 +162,6 @@ local plugins = {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    lazy = false,
     init = function()
       require("telescope").load_extension "todo-comments"
     end,
