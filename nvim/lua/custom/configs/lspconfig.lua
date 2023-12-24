0dc2260145d9afd4fb641e35a200ceec13f31743
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-24 01:10:02 -0800

    Sync configs

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -72,6 +72,7 @@ for _, lsp in ipairs(servers) do
             classRegex = {
               {
                 "Styles \\=([^;]*);",
+                ".*Styles.*=([^;]*);",
                 "Classes \\=([^;]*);",
                 "Variants \\=([^;]*);",
               },
@@ -82,16 +83,16 @@ for _, lsp in ipairs(servers) do
     })
   end
 
-  lspconfig[lsp].setup {
-    opts,
-  }
   -- lspconfig[lsp].setup {
-  --   on_attach = on_attach,
-  --   capabilities = capabilities,
-  --   autotag = {
-  --     enable = true,
-  --   },
+  --   opts,
   -- }
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    autotag = {
+      enable = true,
+    },
+  }
 end
 
 -- go formatting
