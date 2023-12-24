1948636cc87dd46fc8ff16baf57caf522d40fbfd
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-24 01:47:44 -0800

    Sync configs

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -68,17 +68,6 @@ for _, lsp in ipairs(servers) do
     opts = merge_tb(opts, {
       settings = {
         tailwindCSS = {
-          classAttributes = { "class", "className", "class:list", "classList", "ngClass" },
-          lint = {
-            cssConflict = "warning",
-            invalidApply = "error",
-            invalidConfigPath = "error",
-            invalidScreen = "error",
-            invalidTailwindDirective = "error",
-            invalidVariant = "error",
-            recommendedVariantOrder = "warning",
-          },
-          validate = true,
           experimental = {
             classRegex = {
               "Styles =([^;]*);",
