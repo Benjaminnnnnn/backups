af527b46299f15511d940f29ef2f8cf565214367
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-24 15:16:48 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -262,6 +262,16 @@ local plugins = {
       require("cmp").setup(opts)
     end,
   },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
 
 return plugins
