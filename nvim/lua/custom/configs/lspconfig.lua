6d08e28f26a541264879b0837f2a98c074f7c9f9
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-24 01:41:58 -0800

    Sync configs

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -24,7 +24,7 @@ local function organize_imports()
 end
 
 local merge_tb = function(left, right)
-  return vim.tbl_deep_extend("force", vim.deepcopy(left), right)
+  return vim.tbl_deep_extend("force", vim.deepcopy(left), vim.deepcopy(right))
 end
 
 -- configure all lsps
