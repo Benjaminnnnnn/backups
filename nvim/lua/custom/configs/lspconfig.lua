f6550eff92eee5e25fa622feeb2ddac94d53da9d
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-24 00:55:01 -0800

    Sync configs

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -66,7 +66,7 @@ for _, lsp in ipairs(servers) do
         experimental = {
           classRegex = {
             {
-              ".*Styles.* =([^;]*);",
+              ".*Styles.*=([^;]*);",
               ".*Classes \\=([^;]*);",
               ".*Variants \\=([^;]*);",
             },
