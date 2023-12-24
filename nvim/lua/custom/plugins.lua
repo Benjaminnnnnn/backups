7e8c71ba991f031fa24e3b858972a706827e2ee9
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-24 15:31:07 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -272,6 +272,22 @@ local plugins = {
     end,
     ft = { "markdown" },
   },
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.noice"
+    end,
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
 }
 
 return plugins
