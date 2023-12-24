3ee80af1c1f85b8e348aa8737ee6f20e7b6b97fd
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-23 22:57:53 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -165,7 +165,16 @@ local plugins = {
     init = function()
       require "todo-comments"
     end,
-    opts = {},
+    opts = {
+      colors = {
+        error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+        warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+        info = { "DiagnosticInfo", "#2563EB" },
+        hint = { "DiagnosticHint", "#10B981" },
+        default = { "Identifier", "#7C3AED" },
+        test = { "Identifier", "#FF00FF" },
+      },
+    },
     config = function()
       require("todo-comments").setup {}
     end,
