3729317ea687d82c54dfc49a4912c415cfcbadb5
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-25 15:51:40 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -183,7 +183,7 @@ local plugins = {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     cmd = "Trouble",
     config = function()
-      dofile(vim.g.base46_cache .. "trouble")
+      -- dofile(vim.g.base46_cache .. "trouble")
       require("trouble").setup()
     end,
   },
