54853f9f379b55a7f78360d45d2e655bec5d05cd
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-26 09:18:57 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -302,10 +302,8 @@ local plugins = {
     opts = function(_, opts)
       local format_kinds = opts.formatting.format
       opts.formatting.format = function(entry, item)
-        format_kinds(item, entry)
-        return require("tailwindcss-colorizer-cmp").formatter(item, entry)
-        -- format_kinds(entry, item)
-        -- return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+        format_kinds(entry, item)
+        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
       end
       return require "plugins.configs.cmp"
     end,
