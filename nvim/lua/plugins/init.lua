0d50e3d17ecde8fded542585b9ceb45d02fb6a3e
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-25 18:12:48 -0800

    Sync configs

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -35,8 +35,10 @@ local default_plugins = {
       require("core.utils").lazy_load "nvim-colorizer.lua"
     end,
     opts = {
-      rgb_fn = true, -- CSS rgb() and rgba() functions
-      hsl_fn = true, -- CSS hsl() and hsla() functions
+      css = {
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+      },
     },
     config = function(_, opts)
       require("colorizer").setup(opts)
