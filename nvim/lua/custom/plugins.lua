0f9c99bd27a1f4c56056704a289f0210c57a5962
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-25 19:56:02 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -93,13 +93,15 @@ local plugins = {
     end,
   },
 
-  { 'mrshmllow/document-color.nvim', config = function()
-  require("document-color").setup {
-    -- Default options
-    mode = "background", -- "background" | "foreground" | "single"
-  }
-  end
-}
+  {
+    "mrshmllow/document-color.nvim",
+    config = function()
+      require("document-color").setup {
+        -- Default options
+        mode = "background", -- "background" | "foreground" | "single"
+      }
+    end,
+  },
 
   {
     "rcarriga/nvim-dap-ui",
