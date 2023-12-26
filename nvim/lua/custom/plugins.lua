0a6d4d18a19c9593160188606110f37f99fe3de4
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-25 23:31:01 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -258,53 +258,58 @@ local plugins = {
     },
   },
 
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   event = "InsertEnter",
-  --   dependencies = {
-  --     {
-  --       -- snippet plugin
-  --       "L3MON4D3/LuaSnip",
-  --       dependencies = "rafamadriz/friendly-snippets",
-  --       opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-  --       config = function(_, opts)
-  --         require("plugins.configs.others").luasnip(opts)
-  --         require "custom.configs.friendly-snippets"
-  --       end,
-  --     },
-  --
-  --     -- autopairing of (){}[] etc
-  --     {
-  --       "windwp/nvim-autopairs",
-  --       opts = {
-  --         fast_wrap = {},
-  --         disable_filetype = { "TelescopePrompt", "vim" },
-  --       },
-  --       config = function(_, opts)
-  --         require("nvim-autopairs").setup(opts)
-  --
-  --         -- setup cmp for autopairs
-  --         local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-  --         require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  --       end,
-  --     },
-  --
-  --     -- cmp sources plugins
-  --     {
-  --       "saadparwaiz1/cmp_luasnip",
-  --       "hrsh7th/cmp-nvim-lua",
-  --       "hrsh7th/cmp-nvim-lsp",
-  --       "hrsh7th/cmp-buffer",
-  --       "hrsh7th/cmp-path",
-  --     },
-  --   },
-  --   opts = function()
-  --     return require "plugins.configs.cmp"
-  --   end,
-  --   config = function(_, opts)
-  --     require("cmp").setup(opts)
-  --   end,
-  -- },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      {
+        -- snippet plugin
+        "L3MON4D3/LuaSnip",
+        dependencies = "rafamadriz/friendly-snippets",
+        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+        config = function(_, opts)
+          require("plugins.configs.others").luasnip(opts)
+          require "custom.configs.friendly-snippets"
+        end,
+      },
+
+      -- autopairing of (){}[] etc
+      {
+        "windwp/nvim-autopairs",
+        opts = {
+          fast_wrap = {},
+          disable_filetype = { "TelescopePrompt", "vim" },
+        },
+        config = function(_, opts)
+          require("nvim-autopairs").setup(opts)
+
+          -- setup cmp for autopairs
+          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+      },
+
+      -- cmp sources plugins
+      {
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+      },
+    },
+    opts = function(_, opts)
+      local format_kinds = opts.formatting.format
+      opts.formatting.format = function(entry, item)
+        format_kinds(entry, item)
+        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+      end
+      return require "plugins.configs.cmp"
+    end,
+    config = function(_, opts)
+      require("cmp").setup(opts)
+    end,
+  },
 
   {
     "iamcco/markdown-preview.nvim",
