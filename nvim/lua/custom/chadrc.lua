bd097dc6b36d63d7ce5698cc7fb69279d773d500
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-26 21:03:24 -0800

    Sync configs

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -3,7 +3,14 @@ local M = {}
 
 M.ui = {
   theme = "onedark",
-  telescope = { style = "bordered" }, -- borderless / bordered
+  telescope = { style = "borderless" }, -- borderless / bordered
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
+    selected_item_bg = "colored", -- colored / simple
+  },
 }
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
