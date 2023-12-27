c22964a6acf78531dc0605a3439d57d10017d468
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-26 21:01:45 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -105,7 +105,7 @@ local plugins = {
     -- optionally, override the default options:
     config = function()
       require("tailwindcss-colorizer-cmp").setup {
-        color_square_width = 2,
+        -- color_square_width = 2,
       }
     end,
   },
