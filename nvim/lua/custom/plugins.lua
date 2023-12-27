99aadff5e7307e6f246ff7a3f859cbad7ec009f2
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-26 19:16:42 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -85,10 +85,8 @@ local plugins = {
       --   cmp_docs = { always_update = true },
       -- },
       user_default_options = {
-        -- rgb_fn = true, -- CSS rgb() and rgba() functions
-        -- hsl_fn = true, -- CSS hsl() and hsla() functions
         css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB,
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        -- css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
         tailwind = "both",
       },
     },
