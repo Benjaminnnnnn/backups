4ce92f654a9b018587b1d7b5a516d87084d4b204
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-28 08:22:40 -0800

    Sync configs

--- nvim/lua/custom/configs/nvim-treesitter.lua
+++ nvim/lua/custom/configs/nvim-treesitter.lua
@@ -17,6 +17,14 @@ local opts = {
   },
   auto_install = true,
 
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<c-space>",
+      node_incremental = "<c-space>",
+      node_decremental = "<M-space>",
+    },
+  },
   textobjects = {
     select = {
       enable = true,
