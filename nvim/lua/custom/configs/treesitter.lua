42d527eb129b8726d5255c6563c60e14098190d8
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-28 09:26:11 -0800

    Sync configs

--- nvim/lua/custom/configs/treesitter.lua
+++ nvim/lua/custom/configs/treesitter.lua
@@ -31,15 +31,15 @@ local opts = {
     select = {
       enable = true,
       lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      -- keymaps = {
-      --   -- You can use the capture groups defined in textobjects.scm
-      --   ["aa"] = "@parameter.outer",
-      --   ["ia"] = "@parameter.inner",
-      --   ["af"] = "@function.outer",
-      --   ["if"] = "@function.inner",
-      --   ["ac"] = "@class.outer",
-      --   ["ic"] = "@class.inner",
-      -- },
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
     },
   },
 
