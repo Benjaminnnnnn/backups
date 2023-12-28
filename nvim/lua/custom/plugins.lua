a63df146b3dcacd7c35ee1e3a1ec5c9211da1012
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-28 08:33:23 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -23,6 +23,22 @@ local plugins = {
         "json",
       },
       auto_install = true,
+
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+      },
     },
     dependencies = {
       "nvim-treesitter/nvim-treesitter-context",
