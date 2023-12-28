ce322e0f33e5ca826fb2c863019a9a52f144822d
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-28 08:41:12 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -6,41 +6,6 @@ local plugins = {
       return require "custom.configs.treesitter"
     end,
 
-    -- opts = {
-    --   ensure_installed = {
-    --     -- defaults
-    --     "vim",
-    --     "lua",
-    --     "go",
-    --     "rust",
-    --     "python",
-    --
-    --     -- web dev
-    --     "html",
-    --     "css",
-    --     "javascript",
-    --     "typescript",
-    --     "tsx",
-    --     "json",
-    --   },
-    --   auto_install = true,
-    --
-    --   textobjects = {
-    --     select = {
-    --       enable = true,
-    --       lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-    --       keymaps = {
-    --         -- You can use the capture groups defined in textobjects.scm
-    --         ["aa"] = "@parameter.outer",
-    --         ["ia"] = "@parameter.inner",
-    --         ["af"] = "@function.outer",
-    --         ["if"] = "@function.inner",
-    --         ["ac"] = "@class.outer",
-    --         ["ic"] = "@class.inner",
-    --       },
-    --     },
-    --   },
-    -- },
     dependencies = {
       "nvim-treesitter/nvim-treesitter-context",
     },
