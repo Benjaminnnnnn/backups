cb2139ce67d1a331d12242f92a4b58191351722d
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-29 11:55:23 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -203,6 +203,14 @@ local plugins = {
       leap.add_default_mappings(true)
       vim.keymap.del({ "x", "o" }, "x")
       vim.keymap.del({ "x", "o" }, "X")
+      leap.add_repeat_mappings(";", ",", {
+        -- False by default. If set to true, the keys will work like the
+        -- native semicolon/comma, i.e., forward/backward is understood in
+        -- relation to the last motion.
+        relative_directions = true,
+        -- By default, all modes are included.
+        modes = { "n", "x", "o" },
+      })
     end,
   },
 
