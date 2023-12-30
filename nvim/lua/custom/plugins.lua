0513ba675c29417d0ec035bd918fe0b53cd6f843
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-29 18:36:55 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -196,21 +196,22 @@ local plugins = {
       { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
     },
     config = function(_, opts)
-      local leap = require "leap"
-      for k, v in pairs(opts) do
-        leap.opts[k] = v
-      end
-      leap.add_default_mappings(true)
-      leap.add_repeat_mappings(";", ",", {
-        -- False by default. If set to true, the keys will work like the
-        -- native semicolon/comma, i.e., forward/backward is understood in
-        -- relation to the last motion.
-        relative_directions = true,
-        -- By default, all modes are included.
-        modes = { "n", "x", "o" },
-      })
-      vim.keymap.del({ "x", "o" }, "x")
-      vim.keymap.del({ "x", "o" }, "X")
+      require("custom.configs.leap").config(_, opts)
+      -- local leap = require "leap"
+      -- for k, v in pairs(opts) do
+      --   leap.opts[k] = v
+      -- end
+      -- leap.add_default_mappings(true)
+      -- leap.add_repeat_mappings(";", ",", {
+      --   -- False by default. If set to true, the keys will work like the
+      --   -- native semicolon/comma, i.e., forward/backward is understood in
+      --   -- relation to the last motion.
+      --   relative_directions = true,
+      --   -- By default, all modes are included.
+      --   modes = { "n", "x", "o" },
+      -- })
+      -- vim.keymap.del({ "x", "o" }, "x")
+      -- vim.keymap.del({ "x", "o" }, "X")
     end,
   },
 
