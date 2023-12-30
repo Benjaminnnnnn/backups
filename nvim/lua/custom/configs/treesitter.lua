1cd8c9d84ae6597dbe0c6b5be42c3c95e25d83b1
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-30 12:02:25 -0800

    Sync configs

--- nvim/lua/custom/configs/treesitter.lua
+++ nvim/lua/custom/configs/treesitter.lua
@@ -70,11 +70,11 @@ local opts = {
       goto_next_start = {
         ["]m"] = "@function.outer",
         ["]]"] = { query = "@class.outer", desc = "Next class start" },
-        --
+
         -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
         ["]o"] = "@loop.*",
         -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-        --
+
         -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
         -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
         ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
@@ -87,19 +87,15 @@ local opts = {
       goto_previous_start = {
         ["[m"] = "@function.outer",
         ["[["] = "@class.outer",
+
+        ["[o"] = "@loop.*",
+
+        ["[s"] = { query = "@scope", query_group = "locals", desc = "Previous scope" },
+        ["[z"] = { query = "@fold", query_group = "folds", desc = "Previous fold" },
       },
       goto_previous_end = {
         ["[M"] = "@function.outer",
         ["[]"] = "@class.outer",
-      },
-      -- Below will go to either the start or the end, whichever is closer.
-      -- Use if you want more granular movements
-      -- Make it even more gradual by adding multiple queries and regex.
-      goto_next = {
-        ["]d"] = "@conditional.outer",
-      },
-      goto_previous = {
-        ["[d"] = "@conditional.outer",
       },
     },
     swap = {
