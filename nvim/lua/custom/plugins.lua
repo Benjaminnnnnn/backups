bf8ce3bd58d5ceb3b9d8700635cb5fe2267049fa
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-30 16:28:40 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -237,9 +237,9 @@ local plugins = {
       "JoosepAlviste/nvim-ts-context-commentstring",
     },
     opts = {
-      pre_hook = function ()
-      require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      end
+      pre_hook = function()
+        require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+      end,
     },
     -- opts = function()
     --   return {
