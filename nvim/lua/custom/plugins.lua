2aed3470d5e721a1041293bf2dc22e01fddd757c
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-30 16:09:37 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -238,15 +238,29 @@ local plugins = {
     },
   },
 
-  {
+{
     "numToStr/Comment.nvim",
+    keys = {
+      { "gcc", mode = "n", desc = "Comment toggle current line" },
+      { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
+      { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
+      { "gbc", mode = "n", desc = "Comment toggle current block" },
+      { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+      { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
+    },
+    init = function()
+      require("core.utils").load_mappings "comment"
+    end,
+
     opts = {
       -- configure Comment.nvim to trigger the commentstring updating logic with its pre_hook configuration
-      pre_hook = function()
-        require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-      end,
+      pre_hook = 
+        {require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()}
     },
-  },
+    config = function(_, opts)
+      require("Comment").setup(opts)
+    end,
+  }
 
   -- debugger
   {
