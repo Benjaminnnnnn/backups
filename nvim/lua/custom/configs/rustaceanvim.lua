afbcaaff2e1a665f67e36fb6471f74960a239f3d
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2024-01-19 00:35:16 -0800

    Sync configs

--- nvim/lua/custom/configs/rustaceanvim.lua
+++ nvim/lua/custom/configs/rustaceanvim.lua
@@ -1,8 +1,12 @@
 vim.g.rustaceanvim = function()
-  local mason_registry = require "mason-registry"
-  local codelldb_extension = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
-  local codelldb_path = codelldb_extension .. "adapter/codelldb"
-  local liblldb_path = codelldb_extension .. "lldb/lib/liblldb.dylib"
+  -- local mason_registry = require "mason-registry"
+  -- local codelldb_extension = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
+  -- local codelldb_path = codelldb_extension .. "adapter/codelldb"
+  -- local liblldb_path = codelldb_extension .. "lldb/lib/liblldb.dylib"
+
+  local extension_path = vim.fn.expand "~" .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0"
+  local codelldb_path = extension_path .. "/adapter/codelldb"
+  local liblldb_path = extension_path .. "/lldb/lib/liblldb.dylib"
 
   local cfg = require "rustaceanvim.config"
 
