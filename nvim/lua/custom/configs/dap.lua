cc332322dda25245b66eb8e801e792ecdc1f86d1
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2024-01-18 21:18:20 -0800

    Sync configs

--- nvim/lua/custom/configs/dap.lua
+++ nvim/lua/custom/configs/dap.lua
@@ -25,7 +25,6 @@ end
 -- Rust/C/C++
 dap.adapters.codelldb = {
   type = "server",
-  host = "127.0.0.1",
   port = 13000, -- 💀 Use the port printed out or specified with `--port`
 }
 -- dap.adapters.codelldb = {
