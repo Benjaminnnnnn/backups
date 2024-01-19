35e282c987ea34200c519c1414aa1a378b1759fb
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2024-01-18 22:02:46 -0800

    Sync configs

--- nvim/lua/custom/configs/dap.lua
+++ nvim/lua/custom/configs/dap.lua
@@ -34,7 +34,20 @@ dap.adapters.codelldb = {
     -- on windows you may have to uncomment this:
     -- detached = false,
   },
-  options = {
-    initialize_timeout_sec = 10,
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
   },
 }
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
