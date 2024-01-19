40b00aac7b66ccb189fe910b812b13de87193551
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2024-01-18 20:43:48 -0800

    Sync configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -107,6 +107,18 @@ local plugins = {
     end,
   },
 
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end,
+  },
+
   -- UI
   {
     "NvChad/nvim-colorizer.lua",
