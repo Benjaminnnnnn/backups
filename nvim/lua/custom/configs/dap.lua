914b3156321e2d28d139e29667a8804c337e4aa0
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2024-01-18 20:55:40 -0800

    Sync configs

--- nvim/lua/custom/configs/dap.lua
+++ nvim/lua/custom/configs/dap.lua
@@ -21,3 +21,27 @@ for _, language in ipairs { "typescript", "javascript" } do
     },
   }
 end
+
+-- Rust/C/C++
+dap.adapters.codelldb = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = "/usr/bin/codelldb",
+    args = { "--port", "${port}" },
+  },
+}
+
+dap.configurations.rust = {
+  {
+    name = "Rust debug",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = true,
+    showDisassembly = "never",
+  },
+}
