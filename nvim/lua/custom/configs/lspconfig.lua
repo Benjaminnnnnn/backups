b64e8b6a6d15b8695e9b856b486fa9ff9f2e5166
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2024-01-21 05:07:11 +0800

    Sync configs

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -82,6 +82,7 @@ for _, lsp in ipairs(servers) do
   elseif lsp == "rust_analyzer" then
     opts = merge_tb("force", opts, {
       filetypes = { "rust" },
+      root_dir = util.root_pattern("Cargo.toml", "rust-project.json"),
       settings = {
         ["rust-analyzer"] = {
           cargo = {
