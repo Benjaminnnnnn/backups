6771e2bfbbcf879cc830cfb2926cefa6973eac02
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2025-12-03 11:54:54 -0500

    update commit change shell script

--- commit-changes.sh
+++ commit-changes.sh
@@ -10,7 +10,7 @@ rm -rf ./nvim/.git ./nvim/.github ./nvim/.gitignore
 
 # zsh config
 cp $HOME/.zshrc ./zsh/
-cp -r $HOME/.zsh/* ./zsh/
+cp -r $HOME/.zsh/*.sh ./zsh/*.sh
 
 # alacritty config
 cp $HOME/.config/alacritty/alacritty.toml ./alacritty/alacritty.toml
