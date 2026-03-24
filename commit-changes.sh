2858a287e847ba60a8c3b15f9dd74886f867298f
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2026-03-24 12:13:00 -0400

    Fix config sync scripts

--- commit-changes.sh
+++ commit-changes.sh
@@ -1,32 +1,59 @@
 #!/bin/zsh
-cd $(dirname "$0")
+set -euo pipefail
+
+cd -- "$(dirname "$0")"
+
+sync_file() {
+  local src="$1"
+  local dest="$2"
+
+  mkdir -p -- "$(dirname "$dest")"
+  cp -- "$src" "$dest"
+}
+
+sync_directory() {
+  local src="$1"
+  local dest="$2"
+  shift 2
+
+  mkdir -p -- "$dest"
+  /usr/bin/rsync -a --delete "$@" "$src"/ "$dest"/
+}
 
 # tmux config
-cp $HOME/.tmux.conf ./tmux/
+sync_file "$HOME/.tmux.conf" "./tmux/.tmux.conf"
 
 # nvim config
-cp -rf $HOME/.config/nvim/* ./nvim/
-rm -rf ./nvim/.git ./nvim/.github ./nvim/.gitignore
+sync_directory "$HOME/.config/nvim" "./nvim" \
+  --exclude '.git/' \
+  --exclude '.github/' \
+  --exclude '.gitignore'
 
 # zsh config
-cp $HOME/.zshrc ./zsh/
-cp -r $HOME/.zsh/*.sh ./zsh/*.sh
+sync_file "$HOME/.zshrc" "./zsh/.zshrc"
+/usr/bin/rsync -a --delete \
+  --include '*/' \
+  --include '*.sh' \
+  --exclude '*' \
+  "$HOME/.zsh"/ "./zsh"/
 
 # alacritty config
-cp $HOME/.config/alacritty/alacritty.toml ./alacritty/alacritty.toml
+sync_file "$HOME/.config/alacritty/alacritty.toml" "./alacritty/alacritty.toml"
 
 # starship config
-cp $HOME/.config/starship.toml ./starship/starship.toml
+sync_file "$HOME/.config/starship.toml" "./starship/starship.toml"
 
 # yazi config
-cp -r $HOME/.config/yazi ./yazi
+sync_directory "$HOME/.config/yazi" "./yazi"
 
 # ghostty config
-cp $HOME/.config/ghostty/config ./ghostty/config
+sync_file "$HOME/.config/ghostty/config" "./ghostty/config"
 
-# lazygit config
-
-# sync
 git add .
-git commit -m "Sync configs"
-git push origin
+
+if git diff --cached --quiet; then
+  exit 0
+fi
+
+git commit -m "Fix config sync scripts"
+git push origin main
--- sync.sh
+++ sync.sh
@@ -1,3 +1,21 @@
 #!/bin/zsh
-DIR_PATH=$(dirname "$0")
-/usr/local/bin/fswatch -o $HOME/.zshrc $HOME/.zsh/ $HOME/.tmux.conf $HOME/.config/nvim/ $HOME/.config/alacritty/ $HOME/.config/starship.toml $HOME/.config/yazi/ $HOME/.config/ghostty/ | xargs -n1 zsh $DIR_PATH/commit-changes.sh
+set -euo pipefail
+
+DIR_PATH=$(cd -- "$(dirname "$0")" && pwd)
+PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+FSWATCH_BIN=$(command -v fswatch || true)
+
+if [[ -z "$FSWATCH_BIN" ]]; then
+  echo "fswatch not found in PATH: $PATH" >&2
+  exit 1
+fi
+
+"$FSWATCH_BIN" -o \
+  "$HOME/.zshrc" \
+  "$HOME/.zsh/" \
+  "$HOME/.tmux.conf" \
+  "$HOME/.config/nvim/" \
+  "$HOME/.config/alacritty/" \
+  "$HOME/.config/starship.toml" \
+  "$HOME/.config/yazi/" \
+  "$HOME/.config/ghostty/" | xargs -n1 zsh "$DIR_PATH/commit-changes.sh"
